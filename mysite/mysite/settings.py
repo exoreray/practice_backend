1fc9a88e615dd844dac28c343b02392cfd641a35
Rui Xi <xr@berkeley.edu>
2023-01-13 04:06:57 -0800

    finished login regeister api

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listings',
-    'users',
+    'rest_framework',
+    'knox',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'knox.auth.TokenAuthentication',
+    ]
+}
